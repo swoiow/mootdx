eba93e5e4081c650e7df56f8b9ed294a9d09f344
Harmonsir <git@pylab.me>
2025-05-31 16:41:10 +0800

    update version

--- mootdx/__init__.py
+++ mootdx/__init__.py
@@ -1,9 +1,8 @@
 from mootdx import config
-from mootdx.consts import EX_HOSTS
-from mootdx.consts import GP_HOSTS
-from mootdx.consts import HQ_HOSTS
+from mootdx.consts import EX_HOSTS, GP_HOSTS, HQ_HOSTS
 from mootdx.server import server
 from mootdx.utils import get_config_path
 
-__version__ = '0.11.11'
+
+__version__ = '0.11.12'
 __author__ = 'bopo.wang <ibopo@126.com> & HarmonSir <git@pylab.me>'
